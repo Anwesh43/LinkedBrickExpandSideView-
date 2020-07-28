278740997643ae8b0ad055b3418f261ff4bc536c
Anwesh43 <anweshthecool0@gmail.com>
2020-07-29 02:44:57 +0530

    adding BESNode class

--- brickexpandsideview/src/main/java/com/anwesh/uiprojects/brickexpandsideview/BrickExpandSideView.kt
+++ brickexpandsideview/src/main/java/com/anwesh/uiprojects/brickexpandsideview/BrickExpandSideView.kt
@@ -122,4 +122,45 @@ class BrickExpandSideView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BESNode(var i : Int, val state : State = State()) {
+
+        private var next : BESNode? = null
+        private var prev : BESNode? = null
+
+        init {
+           addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BESNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSEBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BESNode {
+            var curr : BESNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
