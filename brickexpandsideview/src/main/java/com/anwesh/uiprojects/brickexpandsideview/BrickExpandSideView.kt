135a49982fd8c58d7a00a06ae3d476ee97b8aa76
Anwesh43 <anweshthecool0@gmail.com>
2020-07-29 02:33:06 +0530

    adding Canvas extension method

--- brickexpandsideview/src/main/java/com/anwesh/uiprojects/brickexpandsideview/BrickExpandSideView.kt
+++ brickexpandsideview/src/main/java/com/anwesh/uiprojects/brickexpandsideview/BrickExpandSideView.kt
@@ -16,7 +16,7 @@ import android.graphics.RectF
 val colors : Array<String> = arrayOf("#3F51B5", "#4CAF50", "#2196F3", "#F44336", "#009688")
 val bricks : Int = 5
 val scGap : Float = 0.02f / bricks
-val sizeFactor : Float = 2f
+val sizeFactor : Float = 2.4f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 
@@ -24,3 +24,38 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawSideExpanderBrick(i : Int, scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, 2)
+    val sf2 : Float = sf.divideScale(1, 2)
+    val sf1i : Float = sf1.divideScale(i, bricks)
+    val sf2i : Float = sf2.divideScale(i, bricks)
+    val gap : Float = h / (bricks + 2)
+    val size : Float = gap / sizeFactor
+    val y : Float = gap * (i + 1)
+    val upSize : Float = size * sf1i
+    save()
+    translate((w / 2 - size) * sf2i, y)
+    drawRect(RectF(-upSize, -size / 2, 0f, size / 2), paint)
+    restore()
+}
+
+fun Canvas.drawSideExpanderBricks(scale : Float, w : Float, h : Float, paint : Paint) {
+    for (j in 0..1) {
+        save()
+        translate(w / 2, 0f)
+        scale(1f - 2 * j, 1f)
+        for (i in 0..(bricks - 1)) {
+            drawSideExpanderBrick(i, scale, w, h, paint)
+        }
+        restore()
+    }
+}
+
+fun Canvas.drawSEBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawSideExpanderBricks(scale, w, h, paint)
+}
