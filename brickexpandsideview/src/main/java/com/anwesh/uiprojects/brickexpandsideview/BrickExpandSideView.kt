4386f3c21635748f5d7d2097488d705f4558da86
Anwesh43 <anweshthecool0@gmail.com>
2020-07-29 02:34:29 +0530

    adding BrickExpandSideView class

--- brickexpandsideview/src/main/java/com/anwesh/uiprojects/brickexpandsideview/BrickExpandSideView.kt
+++ brickexpandsideview/src/main/java/com/anwesh/uiprojects/brickexpandsideview/BrickExpandSideView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawSEBNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     drawSideExpanderBricks(scale, w, h, paint)
 }
+
+class BrickExpandSideView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
