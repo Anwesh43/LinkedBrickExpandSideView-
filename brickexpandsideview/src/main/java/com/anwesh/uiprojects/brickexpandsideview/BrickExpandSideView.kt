67455c5499dda3d7bd0106aa087f32fab54c23cf
Anwesh43 <anweshthecool0@gmail.com>
2020-07-29 02:24:22 +0530

    adding constants and libraries

--- brickexpandsideview/src/main/java/com/anwesh/uiprojects/brickexpandsideview/BrickExpandSideView.kt
+++ brickexpandsideview/src/main/java/com/anwesh/uiprojects/brickexpandsideview/BrickExpandSideView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.brickexpandsideview
 /**
  * Created by anweshmishra on 29/07/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<String> = arrayOf("#3F51B5", "#4CAF50", "#2196F3", "#F44336", "#009688")
+val bricks : Int = 5
+val scGap : Float = 0.02f / bricks
+val sizeFactor : Float = 2f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
